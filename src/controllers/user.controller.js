f076e25c7bcc73adf0f9bc59db3542a5528b9a38
Divyansh Garg <HIMYNAMEISDIVYANSH@GMAIL.COM>
2025-12-24 11:34:55 +0530

    Pipelining shit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -277,4 +277,62 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     );
 });
 
+// Some advanced topics - aggregation, lookup, addFields for SDE-2/3 People :(
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if(!username?.trim()){
+        throw new apiError(400, "Username is required");
+    }
+    const channel = await User.aggregate([
+        {
+            $match: { username: username.toLowerCase() }
+        },
+        { $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },{
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },{
+            $addFields: {
+                subscribersCount: { $size: "$subscribers" },
+                subscribedToCount: { $size: "$subscribedTo" },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [ req.user._id, "$subscribers.subscriber" ] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },{
+            $project: {
+                fullname: 1,
+                username: 1,
+                subscribedToCount: 1,
+                subscribersCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ]);
+
+    if(!channel || channel.length === 0){
+        throw new apiError(404, "Channel not found");
+    }
+    return res.status(200).json(
+        new apiResponse(200, channel[0], "Channel profile fetched successfully")
+    );
+});
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateCoverImage };
